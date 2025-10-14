596ebf72fd9808a1cc4589efe908d7da806d938a
Mohammad Mahfoud <engmohammedmahfoud2001@gmail.com>
2025-10-14 11:03:30 +0300

    install packges needed  for backend

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const cors = require("cors");
 app.use(cors());
 const dotenv = require("dotenv").config();
 app.listen(3000, () => {
-  console.log("this is listen in port 3000");
+  console.log("this is listen in port 300");
 });
